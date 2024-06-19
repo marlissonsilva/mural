65fa6dc5aa9793c192baf222e17d97dab3e506f7
marlissonsilva <marlyssonsilva005@gmail.com>
2024-06-19 16:52:02 -0300

    include New page

---
+++ frontend/src/pages/New.tsx
@@ -0,0 +1,10 @@
+import Form from "@/components/Form";
+import Layout from "@/components/Layout";
+
+export default function New() {
+  return (
+    <Layout title="Adicionar Compra" className="flex flex-col justify-center items-center h-auto sm:h-[95vh]">
+      <Form />
+    </Layout>
+  );
+}
